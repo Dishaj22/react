084357842a6e9303c91fb73c1489c05294700cdf
Dishaj22 <dishajaiswal2301@gmail.com>
2026-09-15 00:11:53 +0530

    Update ProfileCard with props

--- my/src/App.jsx
+++ my/src/App.jsx
@@ -5,11 +5,26 @@ import Main from "./components/Main";
 import Footer from "./components/Footer";
 
 function App() {
+  const user1 = {
+    name: "Disha Jaiswal",
+    age: 22,
+    role: "Software Developer"
+  };
+
+  const user2 = {
+    name: "Alice",
+    age: 28,
+    role: "Frontend Developer"
+  };
+
   return (
     <>
       <h1>Hello World!</h1>
-      <ProfileCard />
-       <Header />
+
+      <ProfileCard {...user1} />
+      <ProfileCard {...user2} />
+
+      <Header />
       <Sidebar />
       <Main />
       <Footer />
--- my/src/ProfileCard.jsx
+++ my/src/ProfileCard.jsx
@@ -1,12 +1,13 @@
-function ProfileCard() {
+function ProfileCard({ name, age, role }) {
   return (
     <div
       className="profile-card"
       style={{ backgroundColor: "#eb9595" }}
     >
       <img src="disha.jpeg" alt="User Profile" />
-      <h2>Disha Jaiswal</h2>
-      <p className="bio">Software Developer</p>
+      <h2>{name}</h2>
+      <p>Age: {age}</p>
+      <p className="bio">{role}</p>
     </div>
   );
 }
