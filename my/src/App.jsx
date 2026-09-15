e4b63932ac098e31edf90065c182de0efeeb0310
Dishaj22 <dishajaiswal2301@gmail.com>
2026-09-15 23:44:26 +0530

    Add ProductCard component

--- my/src/App.jsx
+++ my/src/App.jsx
@@ -6,6 +6,7 @@ import ProfileCard from "./ProfileCard.jsx";
 // import Dashboard from "./Dashboard.jsx";
 import JobBoard from "./JobBorad.jsx";
 import JobCard from "./JobCard.jsx";
+import ProductCard from "./ProductCard.jsx";
 
 // // function App() {
 // //   const user1 = {
@@ -44,11 +45,19 @@ import JobCard from "./JobCard.jsx";
 //     />
 //   );
 // }
-const jobs = [{id: 1,title: "Frontend Dev",company:"TechCrop",location:"Remote",isNew:true},
+// const jobs = [{id: 1,title: "Frontend Dev",company:"TechCrop",location:"Remote",isNew:true},
 
-  {id: 2,title: "Digital",company:"Balaji",location: "office",isNew: true},
+//   {id: 2,title: "Digital",company:"Balaji",location: "office",isNew: true},
 
-]
+// ]
+
+const products = [{
+  id: 101,name:"Wireless Mouse",price:29.99
+},
+{id: 102,name:"Mechanical Keyborad",price:89.99},
+{id: 103,name:"USB-C Hub",price:45.00}
+
+];
 
   
 
@@ -56,9 +65,15 @@ const jobs = [{id: 1,title: "Frontend Dev",company:"TechCrop",location:"Remote",
 function App() {
   return (
     <div>
-      <JobBoard jobs={jobs} />
+      {products.map((product) => (
+        <ProductCard
+          key={product.id}
+          name={product.name}
+          price={product.price}
+        />
+      ))}
     </div>
   );
 }
 
-export default App
+export default App;
